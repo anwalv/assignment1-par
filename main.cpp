c6a6dfcb242ad4d0dd5d63230d3ddd7e898b6662
Anna Oleksandrovych <150846802+anwalv@users.noreply.github.com>
2024-05-26 11:38:29 +0300

    print commands

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    char *text =(char*) calloc(100,sizeof(char));
+    int arraySize = 100;
+    printf("Commands:\n"
+           "1. Append text .\n"
+           "2. Start the new line.\n"
+           "3. Saving information in the file.\n"
+           "4. Loading information from the file.\n"
+           "5. Print current text.\n"
+           "6. Insert the text by line and symbol index.\n"
+           "7. Search text.\n"
+           "8. Exit.\n");
+
 }
