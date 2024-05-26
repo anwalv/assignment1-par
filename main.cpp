ae292f5529f709d1e7b876b8e15e09fc079e5d67
Anna Oleksandrovych <150846802+anwalv@users.noreply.github.com>
2024-05-26 12:06:50 +0300

    print commands

--- main.cpp
+++ main.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void appendText(char *text, int arraySize) {
+    char userInput[1000];
+    printf("Please, enter text:\n");
+    scanf("%999[^\n]", userInput);
+    int inputLength = strlen(userInput);
+    int currentSize = strlen(text);
+    if(currentSize + inputLength >= arraySize){
+        text = (char *)realloc(text, (currentSize + inputLength + 2) * sizeof(char));
+        arraySize = currentSize + inputLength + 2;
+    }
+    strcat(text, userInput);
+    printf("Text was added successfully");
+}
+
+
 
 int main() {
     char *text =(char*) calloc(100,sizeof(char));
@@ -12,5 +31,19 @@ int main() {
            "6. Insert the text by line and symbol index.\n"
            "7. Search text.\n"
            "8. Exit.\n");
-
+    int command;
+    while (command!=8){
+        printf("\nPlease, enter a number of command:");
+        scanf("%d", &command);
+        getchar();
+        if (command < 1 || command > 8) {
+            printf("Invalid command.\n");
+        }
+        if (command == 1){
+            appendText(text, arraySize);
+        }
+    }
+    printf("Ciao!");
+    free(text);
+    return 0;
 }
